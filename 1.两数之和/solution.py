fa02350cc4f12ca06c28db8d3e41b7f0d5aec3a4
starsli <starsli@tencent.com>
2022-11-20 21:48:43 +0800

    save

--- 1.两数之和/solution.py
+++ 1.两数之和/solution.py
@@ -12,5 +12,8 @@ class Solution:
         return result
 
 
+# 遍历nums, 将每个num保存在map中 num(key) -> index(value)
+# 遍历时, 保存sub = target - 当前num, 判断如果sub是map中的key, 说明之前的数据中有一个值sub与当前num 和为target
+
 if __name__ == "__main__":
     print(Solution().twoSum([2, 7, 11, 15], 9))
