b63700689d2c902ded00603308d4d49004788175
starsli <starsli@tencent.com>
2023-06-29 23:06:48 +0800

    tool_server

--- project/tool/functions.py
+++ project/tool/functions.py
@@ -1,4 +1,5 @@
 import base64
+import hashlib
 
 button_to_function = {}
 
@@ -10,12 +11,16 @@ def base64_decode(input_text):
     output_text = base64.b64decode(input_text.encode('utf-8')).decode('utf-8')
     return output_text
 
+def calc_md5(input_text):
+    m = hashlib.md5()
+    m.update(input_text.encode('utf-8'))
+    output_text = m.hexdigest()
+    return output_text
+
 def register():
     button_to_function["Encode"] = base64_encode
     button_to_function["Decode"] = base64_decode
+    button_to_function["MD5"] = calc_md5
 
 def run(input_text, submit_button):
-    print(input_text)
-    print(submit_button)
-    print(button_to_function[submit_button])
     return button_to_function[submit_button](input_text)
