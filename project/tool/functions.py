effe66c6fc085398d0fbc4722b0713c71286e8d5
starsli <starsli@tencent.com>
2023-06-29 23:32:08 +0800

    tool_server

--- project/tool/functions.py
+++ project/tool/functions.py
@@ -3,13 +3,16 @@ import hashlib
 
 button_to_function = {}
 
+
 def base64_encode(input_text):
     output_text = base64.b64encode(input_text.encode('utf-8')).decode('utf-8')
     return output_text
 
+
 def base64_decode(input_text):
     output_text = base64.b64decode(input_text.encode('utf-8')).decode('utf-8')
     return output_text
+
 
 def calc_md5(input_text):
     m = hashlib.md5()
@@ -17,13 +20,17 @@ def calc_md5(input_text):
     output_text = m.hexdigest()
     return output_text
 
-def add_button(button, function):
-    button_to_function[button] = function
 
+def add_button(button_name, function):
+    button_to_function[button_name] = function
+
+
+# 增加一个工具按钮,只需要实现在register注册一个按钮, 不需要修改html文件
 def register():
     add_button("Encode", base64_encode)
     add_button("Decode", base64_decode)
     add_button("MD5", calc_md5)
+
 
 def get_buttons():
     return button_to_function
